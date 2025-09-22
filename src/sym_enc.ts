3b01646044f421fbe80f2c9e230ea4231a1f4759
gram <git@orsinium.dev>
2025-09-22 13:04:18 +0200

    SymEnc.armorEncrypted

--- src/sym_enc.ts
+++ src/sym_enc.ts
@@ -42,4 +42,41 @@ export namespace SymEnc {
     ]);
     return { encrypt: key, decrypt: key };
   }
+
+  export async function armorEncrypted(
+    pair: Types.EncPair,
+    encKey: Types.EncryptKey,
+    salt: Types.Salt
+  ): Promise<string> {
+    const plainBytes = await crypto.subtle.exportKey("raw", pair.encrypt);
+    let iv = salt.salt.slice(0, 16);
+    const encBytes = await crypto.subtle.encrypt(
+      { name: encKey.encrypt.algorithm.name, iv },
+      encKey.encrypt,
+      plainBytes
+    );
+    return arrayBufferToBase64(encBytes);
+  }
+
+  export async function dearmorEncrypted(
+    base64: string,
+    decKey: Types.DecryptKey,
+    salt: Types.Salt
+  ): Promise<Types.EncPair> {
+    const encBytes = base64ToArrayBuffer(base64);
+    let iv = salt.salt.slice(0, 16);
+    const plainBytes = await crypto.subtle.decrypt(
+      { name: decKey.decrypt.algorithm.name, iv },
+      decKey.decrypt,
+      encBytes
+    );
+    const key = await crypto.subtle.importKey(
+      "raw",
+      plainBytes,
+      { name: "AES-GCM" },
+      true,
+      ["encrypt", "decrypt"]
+    );
+    return { encrypt: key, decrypt: key };
+  }
 }
