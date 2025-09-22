25c2ab77687688433818c35facb91ee97e1e7e3a
gram <git@orsinium.dev>
2025-09-22 12:17:01 +0200

    VerifyKey

---
+++ src/verify_key.ts
@@ -0,0 +1,25 @@
+import { Types } from "./types";
+import { arrayBufferToBase64, base64ToArrayBuffer } from "./utils";
+
+export namespace VerifyKey {
+  export function fromSignPair(pair: Types.SignPair): Types.VerifyKey {
+    return { verify: pair.verify };
+  }
+
+  export async function armor(key: Types.VerifyKey): Promise<string> {
+    const bytes = await crypto.subtle.exportKey("spki", key.verify);
+    return arrayBufferToBase64(bytes);
+  }
+
+  export async function dearmor(base64: string): Promise<Types.VerifyKey> {
+    const bytes = base64ToArrayBuffer(base64);
+    const key = await crypto.subtle.importKey(
+      "spki",
+      bytes,
+      { name: "RSA-PSS", hash: "SHA-256" },
+      true,
+      ["verify"]
+    );
+    return { verify: key };
+  }
+}
