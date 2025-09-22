adf29d28033b390496ccda3e301c85b9bec47427
gram <git@orsinium.dev>
2025-09-22 12:27:57 +0200

    EncPair.armor

--- src/enc_pair.ts
+++ src/enc_pair.ts
@@ -1,4 +1,5 @@
 import { Types } from "./types";
+import { arrayBufferToBase64, base64ToArrayBuffer } from "./utils";
 
 export namespace EncPair {
   export async function derive(
@@ -25,6 +26,20 @@ export namespace EncPair {
       true,
       ["encrypt", "decrypt"]
     );
+    return { encrypt: key, decrypt: key };
+  }
+
+  export async function armor(pair: Types.EncPair): Promise<string> {
+    const bytes = await crypto.subtle.exportKey("raw", pair.encrypt);
+    return arrayBufferToBase64(bytes);
+  }
+
+  export async function dearmor(base64: string): Promise<Types.EncPair> {
+    const bytes = base64ToArrayBuffer(base64);
+    const key = await crypto.subtle.importKey("raw", bytes, { name: "AES-GCM" }, true, [
+      "encrypt",
+      "decrypt",
+    ]);
     return { encrypt: key, decrypt: key };
   }
 }
