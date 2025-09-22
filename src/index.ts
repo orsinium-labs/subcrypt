7b739a207cec31e7ca6c73e9aa12deb5c3f95107
gram <git@orsinium.dev>
2025-09-22 13:17:21 +0200

    jest

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Salt } from "./salt";
+export { hashString } from "./hash";
+export { SignKey } from "./sign_key";
+export { SignPair } from "./sign_pair";
+export { SymEnc } from "./sym_enc";
+export { Types } from "./types";
+export { VerifyKey } from "./verify_key";
--- src/salt.ts
+++ src/salt.ts
@@ -5,4 +5,12 @@ export namespace Salt {
     const salt = crypto.getRandomValues(new Uint8Array(16));
     return { salt };
   }
+
+  export function fromArray(arr: number[]): Types.Salt {
+    return fromUint8Array(new Uint8Array(arr));
+  }
+
+  export function fromUint8Array(arr: Uint8Array<ArrayBuffer>): Types.Salt {
+    return { salt: arr };
+  }
 }
